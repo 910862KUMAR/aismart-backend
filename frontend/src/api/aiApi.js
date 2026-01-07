d783e1e5112b166732d8fae64dac3f8887ceab4f
910862KUMAR <145100981+910862KUMAR@users.noreply.github.com>
2026-01-07 18:14:48 +0530

    Delete frontend directory

--- frontend/src/api/aiApi.js
+++
@@ -1,8 +0,0 @@
-import axiosClient from "./axiosClient";
-
-export const askAi = async (message) => {
-  const response = await axiosClient.post("/ai/chat", {
-    message,
-  });
-  return response.data;
-};
--- frontend/src/api/authApi.js
+++
@@ -1,6 +0,0 @@
-import axiosClient from "./axiosClient";
-
-export const loginApi = async (credentials) => {
-  const res = await axiosClient.post("/auth/login", credentials);
-  return res.data;
-};
